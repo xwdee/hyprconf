bf2336366dfc831dfc353937954f072b4f8ac9f0
Xunta <nameerror266@gmail.com>
2026-05-13 20:02:54 +0300

    Add Some Configs...

---
+++ kitty/scroll_mark.py
@@ -0,0 +1,20 @@
+# from: https://github.com/trygveaa/kitty-kitten-search
+
+from kittens.tui.handler import result_handler
+from kitty.boss import Boss
+
+
+def main(args: list[str]) -> None:
+    pass
+
+
+@result_handler(no_ui=True)
+def handle_result(
+    args: list[str], answer: str, target_window_id: int, boss: Boss
+) -> None:
+    w = boss.window_id_map.get(target_window_id)
+    if w is not None:
+        if len(args) > 1 and args[1] != "prev":
+            w.scroll_to_mark(prev=False)
+        else:
+            w.scroll_to_mark()
